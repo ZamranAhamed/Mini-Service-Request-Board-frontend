b4894527b2a8779d8dd37a06af27c747f45755a7
Zamran Ahamed <zamranahamed7@gmail.com>
2026-05-17 15:07:12 +0530

    Fix production API routes

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://globaltna-backend-hq7l.onrender.com",
+  baseURL: "https://globaltna-backend-hq7l.onrender.com/api",
 });
 
 export const getJobs = (filters = {}) => {
   const params = {};
 
-  if (filters.category) {
-    params.category = filters.category;
-  }
-
-  if (filters.status) {
-    params.status = filters.status;
-  }
-
-  if (filters.search) {
-    params.search = filters.search;
-  }
+  if (filters.category) params.category = filters.category;
+  if (filters.status) params.status = filters.status;
+  if (filters.search) params.search = filters.search;
 
   return api.get("/jobs", { params });
 };
@@ -27,7 +19,7 @@ export const getJobById = (id) => api.get(`/jobs/${id}`);
 export const createJob = (data) => api.post("/jobs", data);
 
 export const updateJobStatus = (id, status) =>
-  api.patch(`/jobs/${id}/status`, { status });
+  api.patch(`/jobs/${id}`, { status });
 
 export const deleteJob = (id) => api.delete(`/jobs/${id}`);
 
